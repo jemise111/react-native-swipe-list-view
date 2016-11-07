3b6d253be9d2dd738b7fceeab8f59b8812f5c2ed
Levi Tan Ong <levi@byimplication.com>
2016-11-08 02:17:58 +0800

    onRowDidOpen in swipe-list-view

--- components/SwipeListView.js
+++ components/SwipeListView.js
@@ -76,6 +76,7 @@ class SwipeListView extends Component {
 					...Component.props,
 					ref: row => this._rows[`${secId}${rowId}`] = row,
 					onRowOpen: _ => this.onRowOpen(secId, rowId, this._rows),
+          onRowDidOpen: _ => this.props.onRowDidOpen && this.props.onRowDidOpen(secId, rowId, this._rows)
 					onRowClose: _ => this.props.onRowClose && this.props.onRowClose(secId, rowId, this._rows),
           onRowDidClose: _ => this.props.onRowDidClose && this.props.onRowDidClose(secId, rowId, this._rows),
 					onRowPress: _ => this.onRowPress(`${secId}${rowId}`),
@@ -88,6 +89,7 @@ class SwipeListView extends Component {
 				<SwipeRow
 					ref={row => this._rows[`${secId}${rowId}`] = row}
 					onRowOpen={ _ => this.onRowOpen(secId, rowId, this._rows) }
+          onRowDidOpen={ _ => this.props.onRowDidOpen && this.props.onRowDidOpen(secId, rowId, this._rows)}
 					onRowClose={ _ => this.props.onRowClose && this.props.onRowClose(secId, rowId, this._rows) }
           onRowDidClose={ _ => this.props.onRowDidClose && this.props.onRowDidClose(secId, rowId, this._rows) }
 					onRowPress={ _ => this.onRowPress(`${secId}${rowId}`) }
@@ -171,7 +173,11 @@ SwipeListView.propTypes = {
 	 * Called when a swipe row is animating open
 	 */
 	onRowOpen: PropTypes.func,
-	/**
+  /**
+	 * Called when a swipe row has animated open
+	 */
+	onRowDidOpen: PropTypes.func,
+  /**
 	 * Called when a swipe row is animating closed
 	 */
   onRowClose: PropTypes.func,
