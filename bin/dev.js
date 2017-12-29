03721c2640a76f3c47d65288ed8d116351b09565
Jesse Sessler <jesse.sessler@gmail.com>
2017-12-29 08:56:43 +0700

    Add script to work on

---
+++ bin/dev.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+fs.copyFileSync('components/SwipeListView.js', 'SwipeListExample/SwipeListView.js');
+fs.copyFileSync('components/SwipeRow.js', 'SwipeListExample/SwipeRow.js');
+
+fs.readFile('SwipeListExample/example.js', 'utf8', (err, data) => {
+    const result = data.replace(
+        "import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';",
+        "import SwipeListView from './SwipeListView';\nimport SwipeRow from './SwipeRow';"
+    );
+
+    fs.writeFile('SwipeListExample/example.js', result, 'utf8', (err) => {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
