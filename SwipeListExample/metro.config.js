421bcdf192a1d2e2d36d20a63947c594ffa4ebb8
Jesse Sessler <jesse.sessler@gmail.com>
2019-10-05 10:08:16 -0500

    Add eslint, prettier, fix all files

--- SwipeListExample/metro.config.js
+++ SwipeListExample/metro.config.js
@@ -7,18 +7,17 @@
 const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-  resolver: {
-    blacklistRE: blacklist([
-        /node_modules\/.*\/node_modules\/react-native\/.*/,
-    ])
-  },
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: false,
+            },
+        }),
+    },
+    resolver: {
+        blacklistRE: blacklist([
+            /node_modules\/.*\/node_modules\/react-native\/.*/,
+        ]),
+    },
 };
-
