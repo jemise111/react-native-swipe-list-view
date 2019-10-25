9ab3acce4eb856291c1022a4f00f5d484979b87b
Daniel Syme <daniel.syme@sportsbet.com.au>
2019-10-25 14:58:44 +1100

    add accessible false to touchableopacity
    so that individual elements inside the visible row are accessible

--- components/SwipeRow.js
+++ components/SwipeRow.js
@@ -415,7 +415,11 @@ class SwipeRow extends Component {
         }
 
         return (
-            <TouchableOpacity activeOpacity={1} onPress={this.combinedOnPress}>
+            <TouchableOpacity
+                activeOpacity={1}
+                onPress={this.combinedOnPress}
+                accessible={false}
+            >
                 {this.props.children[1]}
             </TouchableOpacity>
         );
