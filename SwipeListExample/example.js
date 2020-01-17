a5b9f51a4f5ee6b6ea614ffbeabcc4e5887aae00
proof666 <23525436+proof666@users.noreply.github.com>
2020-01-17 23:35:08 +0300

    Add actions example

--- SwipeListExample/example.js
+++ SwipeListExample/example.js
@@ -10,7 +10,130 @@ import {
     View,
 } from 'react-native';
 
-import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';
+import SwipeListView from './SwipeListView';
+import SwipeRow from './SwipeRow';
+
+const HiddenItemWithActions = props => {
+    const {
+        leftActionActivated,
+        rightActionActivated,
+        rowSwipeAnimatedValue,
+        rowActionAnimatedValue,
+        rowHeightAnimatedValue,
+        onClose,
+        onDelete,
+    } = props;
+
+    if (rightActionActivated) {
+        Animated.spring(rowActionAnimatedValue, {
+            toValue: 500,
+        }).start();
+    } else {
+        Animated.spring(rowActionAnimatedValue, {
+            toValue: 75,
+        }).start();
+    }
+
+    return (
+        <Animated.View
+            style={[
+                styles.rowBack,
+                { height: rowHeightAnimatedValue },
+                leftActionActivated && { backgroundColor: 'lightgreen' },
+            ]}
+        >
+            <Text>Left</Text>
+            {!leftActionActivated && (
+                <TouchableOpacity
+                    style={[styles.backRightBtn, styles.backRightBtnLeft]}
+                    onPress={onClose}
+                >
+                    <Text style={styles.backTextWhite}>Close</Text>
+                </TouchableOpacity>
+            )}
+            {!leftActionActivated && (
+                <Animated.View
+                    style={[
+                        styles.backRightBtn,
+                        styles.backRightBtnRight,
+                        { flex: 1, width: rowActionAnimatedValue },
+                    ]}
+                >
+                    <TouchableOpacity
+                        style={[styles.backRightBtn, styles.backRightBtnRight]}
+                        onPress={onDelete}
+                    >
+                        <Animated.View
+                            style={[
+                                styles.trash,
+                                {
+                                    transform: [
+                                        {
+                                            scale: rowSwipeAnimatedValue.interpolate(
+                                                {
+                                                    inputRange: [45, 90],
+                                                    outputRange: [0, 1],
+                                                    extrapolate: 'clamp',
+                                                }
+                                            ),
+                                        },
+                                    ],
+                                },
+                            ]}
+                        >
+                            <Image
+                                source={require('./images/trash.png')}
+                                style={styles.trash}
+                            />
+                        </Animated.View>
+                    </TouchableOpacity>
+                </Animated.View>
+            )}
+        </Animated.View>
+    );
+};
+
+const VisibleItem = props => {
+    const {
+        rowHeightAnimatedValue,
+        rightActionEvaluated,
+        leftActionEvaluated,
+        data,
+        removeRow,
+    } = props;
+
+    if (rightActionEvaluated) {
+        Animated.timing(rowHeightAnimatedValue, {
+            toValue: 0,
+            duration: 200,
+        }).start(() => {
+            removeRow();
+        });
+    }
+
+    return (
+        <Animated.View
+            style={[
+                styles.rowFront,
+                { height: rowHeightAnimatedValue },
+                leftActionEvaluated && { backgroundColor: 'lightgreen' },
+            ]}
+        >
+            <TouchableHighlight
+                onPress={() => console.log('You touched me')}
+                style={[
+                    styles.rowFront,
+                    leftActionEvaluated && { backgroundColor: 'lightgreen' },
+                ]}
+                underlayColor={'#AAA'}
+            >
+                <View>
+                    <Text>I am {data.item.text} in a SwipeListView</Text>
+                </View>
+            </TouchableHighlight>
+        </Animated.View>
+    );
+};
 
 class App extends Component {
     constructor(props) {
@@ -74,6 +197,22 @@ class App extends Component {
         console.log('This row opened', rowKey);
     };
 
+    onLeftActionStatusChange = rowKey => {
+        console.log('onLeftActionStatusChange', rowKey);
+    };
+
+    onRightActionStatusChange = rowKey => {
+        console.log('onRightActionStatusChange', rowKey);
+    };
+
+    onRightAction = rowKey => {
+        console.log('onRightAction', rowKey);
+    };
+
+    onLeftAction = rowKey => {
+        console.log('onLeftAction', rowKey);
+    };
+
     onSwipeValueChange = swipeData => {
         const { key, value } = swipeData;
         this.rowSwipeAnimatedValues[key].setValue(Math.abs(value));
@@ -96,27 +235,29 @@ class App extends Component {
 
                 <View style={styles.controls}>
                     <View style={styles.switchContainer}>
-                        {['FlatList', 'Advanced', 'SectionList'].map(type => (
-                            <TouchableOpacity
-                                key={type}
-                                style={[
-                                    styles.switch,
-                                    {
-                                        backgroundColor:
-                                            this.state.listType === type
-                                                ? 'grey'
-                                                : 'white',
-                                    },
-                                ]}
-                                onPress={() =>
-                                    this.setState({ listType: type })
-                                }
-                            >
-                                <Text>{type}</Text>
-                            </TouchableOpacity>
-                        ))}
+                        {['FlatList', 'Per row', 'Actions', 'SectionList'].map(
+                            type => (
+                                <TouchableOpacity
+                                    key={type}
+                                    style={[
+                                        styles.switch,
+                                        {
+                                            backgroundColor:
+                                                this.state.listType === type
+                                                    ? 'grey'
+                                                    : 'white',
+                                        },
+                                    ]}
+                                    onPress={() =>
+                                        this.setState({ listType: type })
+                                    }
+                                >
+                                    <Text>{type}</Text>
+                                </TouchableOpacity>
+                            )
+                        )}
                     </View>
-                    {this.state.listType === 'Advanced' && (
+                    {this.state.listType === 'Per row' && (
                         <Text>(per row behavior)</Text>
                     )}
                 </View>
@@ -202,7 +343,7 @@ class App extends Component {
                     />
                 )}
 
-                {this.state.listType === 'Advanced' && (
+                {this.state.listType === 'Per row' && (
                     <SwipeListView
                         data={this.state.listViewData}
                         renderItem={(data, rowMap) => (
@@ -261,6 +402,73 @@ class App extends Component {
                                 </TouchableHighlight>
                             </SwipeRow>
                         )}
+                    />
+                )}
+
+                {this.state.listType === 'Actions' && (
+                    <SwipeListView
+                        data={this.state.listViewData}
+                        renderItem={(data, rowMap) => {
+                            const rowHeightAnimatedValue = new Animated.Value(
+                                50
+                            );
+                            return (
+                                <VisibleItem
+                                    rowHeightAnimatedValue={
+                                        rowHeightAnimatedValue
+                                    }
+                                    data={data}
+                                    removeRow={() =>
+                                        this.deleteRow(rowMap, data.item.key)
+                                    }
+                                />
+                            );
+                        }}
+                        renderHiddenItem={(data, rowMap) => {
+                            const rowActionAnimatedValue = new Animated.Value(
+                                75
+                            );
+                            const rowHeightAnimatedValue = new Animated.Value(
+                                50
+                            );
+                            return (
+                                <HiddenItemWithActions
+                                    data={data}
+                                    rowMap={rowMap}
+                                    rowActionAnimatedValue={
+                                        rowActionAnimatedValue
+                                    }
+                                    rowHeightAnimatedValue={
+                                        rowHeightAnimatedValue
+                                    }
+                                    rowSwipeAnimatedValue={
+                                        this.rowSwipeAnimatedValues[
+                                        data.item.key
+                                        ]
+                                    }
+                                    onClose={() =>
+                                        this.closeRow(rowMap, data.item.key)
+                                    }
+                                    onDelete={() =>
+                                        this.deleteRow(rowMap, data.item.key)
+                                    }
+                                />
+                            );
+                        }}
+                        onRowDidOpen={this.onRowDidOpen}
+                        onSwipeValueChange={this.onSwipeValueChange}
+                        leftOpenValue={75}
+                        rightOpenValue={-150}
+                        leftActivationValue={100}
+                        rightActivationValue={-200}
+                        leftActionValue={0}
+                        rightActionValue={-500}
+                        onLeftAction={this.onLeftAction}
+                        onRightAction={this.onRightAction}
+                        onLeftActionStatusChange={this.onLeftActionStatusChange}
+                        onRightActionStatusChange={
+                            this.onRightActionStatusChange
+                        }
                     />
                 )}
 
@@ -364,6 +572,7 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         justifyContent: 'center',
         height: 50,
+        width: '100%',
     },
     rowBack: {
         alignItems: 'center',
@@ -374,12 +583,13 @@ const styles = StyleSheet.create({
         paddingLeft: 15,
     },
     backRightBtn: {
-        alignItems: 'center',
+        alignItems: 'flex-end',
         bottom: 0,
         justifyContent: 'center',
         position: 'absolute',
         top: 0,
         width: 75,
+        paddingRight: 17,
     },
     backRightBtnLeft: {
         backgroundColor: 'blue',
@@ -408,6 +618,7 @@ const styles = StyleSheet.create({
     trash: {
         height: 25,
         width: 25,
+        marginRight: 7,
     },
 });
 
