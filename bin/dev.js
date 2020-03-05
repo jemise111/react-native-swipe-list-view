0f2aa30a115b1796a1948bf7d8da909e7626a420
Jesse Sessler <jesse.sessler@gmail.com>
2020-03-04 21:34:06 -0600

    Fix dev script for new file structure

--- bin/dev.js
+++ bin/dev.js
@@ -8,15 +8,41 @@ fs.copyFileSync(
 );
 fs.copyFileSync('components/SwipeRow.js', 'SwipeListExample/SwipeRow.js');
 
-fs.readFile('SwipeListExample/example.js', 'utf8', (err, data) => {
-    const result = data.replace(
-        "import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';",
-        "import SwipeListView from './SwipeListView';\nimport SwipeRow from './SwipeRow';"
-    );
+fs.readdir('SwipeListExample/examples', (err, files) => {
+    if (err) {
+        return console.log(err);
+    }
+    files.forEach(fileName => {
+        const fullPath = `SwipeListExample/examples/${fileName}`;
+        swapImports(fullPath);
+    });
+});
 
-    fs.writeFile('SwipeListExample/example.js', result, 'utf8', err => {
+const swapImports = path => {
+    fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
             return console.log(err);
         }
+
+        let result = data.replace(
+            "import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';",
+            "import SwipeListView from '../SwipeListView';\nimport SwipeRow from '../SwipeRow';"
+        );
+
+        result = result.replace(
+            "import { SwipeListView } from 'react-native-swipe-list-view';",
+            "import SwipeListView from '../SwipeListView';"
+        );
+
+        result = result.replace(
+            "import { SwipeRow } from 'react-native-swipe-list-view';",
+            "import SwipeRow from '../SwipeRow';"
+        );
+
+        fs.writeFile(path, result, 'utf8', err => {
+            if (err) {
+                return console.log(err);
+            }
+        });
     });
-});
+};
